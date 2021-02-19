4f249b55129e2029cd33519d5c334c55fa39913b
galbeiroc <alcresgu_1989@hotmail.com>
2021-02-19 14:28:14 -0500

    05-Change URL prod

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'devs-news-acg-37zmjo023.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://devs-news-acg.galbeiroc.vercel.app'
