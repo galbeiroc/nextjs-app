c339e0a9d3aeca3a95c39649a44350002e26deea
galbeiroc <alcresgu_1989@hotmail.com>
2021-02-19 14:14:46 -0500

    03-add domian

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://myDominio.com'
+export const server = dev ? 'http://localhost:3000' : 'devs-news-acg-37zmjo023.vercel.app'
